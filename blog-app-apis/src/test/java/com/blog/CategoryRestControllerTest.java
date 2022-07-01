8840a16ff85a6695aeae1d16970aab21b88a173b
Gaurab <gaurabacharya1@gmail.com>
2022-07-01 16:40:41 +0545

    working mockito testing

--- blog-app-apis/src/test/java/com/blog/CategoryRestControllerTest.java
+++ blog-app-apis/src/test/java/com/blog/CategoryRestControllerTest.java
@@ -3,9 +3,9 @@ package com.blog;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -22,11 +22,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest(classes = CategoryController.class)
 class CategoryRestControllerTest {
 
 	@MockBean // similar to @Autowired
 	private CategoryService categoryService;
+	
+	@InjectMocks
+	private CategoryController categoryController;
 
 	@Autowired
 	private MockMvc mockMvc; // mock mvc is used to send the request.
